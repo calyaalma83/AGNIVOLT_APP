8043a6f788cde49531526e2816957793b52181d8
calyaalma83 <kacangalmond83@gmail.com>
2025-10-31 19:30:06 +0700

    ...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     authDomain: "agnivolt-c61a6.firebaseapp.com", 
     databaseURL: "https://agnivolt-c61a6-default-rtdb.asia-southeast1.firebasedatabase.app", 
     projectId: "agnivolt-c61a6", 
-    storageBucket: "agnivolt-c61a6.firebasestorage.app", 
+    storageBucket: "agnivolt-c61a6.appspot.com", 
     messagingSenderId: "453833599474", 
     appId: "1:453833599474:web:bc3e76ca1deeea0969a36b"
   }
